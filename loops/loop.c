7fc1b4afa869c9caa0742c0ede596a5b78c3bc69
MANISHIMWESalton <manishimwejosephs@gmail.com>
2026-03-23 07:58:26 +0200

    treval loop

--- loops/loop.c
+++ loops/loop.c
@@ -18,5 +18,13 @@ int main(){
         printf("Counted number: %d \n",count);
         count++;
     }
+    int total = 0;
+    int number;
+    printf("Enter 5 numbers:");
+    for(int i = 1;i<=5;i++){
+        scanf("%d",&number);
+        total += number;
+    }
+    printf(" The Total number = %d \n",total);
     return 0;
 }
